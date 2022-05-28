017e1eb1da36a21196573f85ed69894294a3cf22
Nino Filiu <nino.filiu@gmail.com>
2022-05-28 09:24:53 +0200

    viz: xy

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import untypedData from "./data.json";
 
 const width = 500;
 const height = 500;
-const saveImage = false;
+const savePrefix = "xy";
 
 const data = untypedData as (number | null)[][][];
 let source = 0;
-let column = 0;
+let cx = 0;
+let cy = 0;
 
 const canvas = document.createElement("canvas");
 canvas.width = width;
@@ -18,28 +19,39 @@ const isNumberArray = (values: (number | null)[]): values is number[] =>
   !values.some((value) => value === null);
 
 const update = () => {
-  const values = data[source].map((row) => row[column]);
-  if (!isNumberArray(values)) return console.log("null detected");
-  const min = Math.min(...values);
-  const max = Math.max(...values);
-  if (min === max) return console.log("all same");
-  const points = values.map(
-    (v, i) =>
-      [
-        (width * i) / (values.length - 1),
-        (height * (v - min)) / (max - min),
-      ] as const
-  );
+  const m = data[source];
+  const xs = m.map((row) => row[cx]);
+  if (!isNumberArray(xs)) return console.log("null detected");
+  const xmin = Math.min(...xs);
+  const xmax = Math.max(...xs);
+  if (xmin === xmax) return console.log("all same");
+
+  const ys = m.map((row) => row[cy]);
+  if (!isNumberArray(ys)) return console.log("null detected");
+  const ymin = Math.min(...ys);
+  const ymax = Math.max(...ys);
+  if (ymin === ymax) return console.log("all same");
+
+  const ps = Array(m.length)
+    .fill(null)
+    .map(
+      (_, i) =>
+        [
+          width * ((xs[i] - xmin) / (xmax - xmin)),
+          height * ((ys[i] - ymin) / (ymax - ymin)),
+        ] as const
+    );
+
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, width, height);
   ctx.strokeStyle = "black";
   ctx.beginPath();
-  points.forEach((p, i) => (i === 0 ? ctx.moveTo(...p) : ctx.lineTo(...p)));
+  ps.forEach((p, i) => (i === 0 ? ctx.moveTo(...p) : ctx.lineTo(...p)));
   ctx.stroke();
 
-  if (saveImage) {
+  if (savePrefix) {
     const a = document.createElement("a");
-    a.download = `0-${source}-${column}.png`;
+    a.download = `${savePrefix}.${source}-${cx}-${cy}.png`;
     a.href = canvas.toDataURL();
     a.click();
   }
@@ -56,19 +68,30 @@ sourceInput.addEventListener("input", () => {
   update();
 });
 
-const columnInput = document.createElement("input");
-columnInput.type = "number";
-columnInput.min = "0";
-columnInput.step = "1";
-columnInput.max = `${data[0].length - 1}`;
-columnInput.value = `${column}`;
-columnInput.addEventListener("input", () => {
-  column = +columnInput.value;
+const cxInput = document.createElement("input");
+cxInput.type = "number";
+cxInput.min = "0";
+cxInput.step = "1";
+cxInput.max = `${data[0].length - 1}`;
+cxInput.value = `${cx}`;
+cxInput.addEventListener("input", () => {
+  cx = +cxInput.value;
+  update();
+});
+
+const cyInput = document.createElement("input");
+cyInput.type = "number";
+cyInput.min = "0";
+cyInput.step = "1";
+cyInput.max = `${data[0].length - 1}`;
+cyInput.value = `${cy}`;
+cyInput.addEventListener("input", () => {
+  cy = +cyInput.value;
   update();
 });
 
 const header = document.createElement("div");
-header.append(sourceInput, columnInput);
+header.append(sourceInput, cxInput, cyInput);
 document.body.append(header, canvas);
 
 update();
