f148856280b291ba1ecca5c08be6420dd2d705f3
Nino Filiu <nino.filiu@gmail.com>
2022-05-29 18:38:00 +0200

    blur

--- src/main.ts
+++ src/main.ts
@@ -2,34 +2,44 @@ import untypedData from "./data.json";
 
 const width = 500;
 const height = 500;
+const blur = 3;
+let source: number;
+let cx: number;
+let cy: number;
 
 const data = untypedData as number[][][];
 
 const canvas = document.createElement("canvas");
 canvas.width = width;
 canvas.height = height;
-canvas.style.border = "1px solid black";
 const ctx = canvas.getContext("2d")!;
+ctx.lineWidth = 2 * blur;
+ctx.filter = `blur(${blur}px) contrast(5)`;
 document.body.append(canvas);
 
-const getNormalizedValues = (source: number, col: number) => {
-  const rawValues = data[source][col];
-  const min = Math.min(...rawValues);
-  const max = Math.max(...rawValues);
-  return rawValues.map((v) => (v - min) / (max - min));
+const normalize = (values: number[]) => {
+  const min = Math.min(...values);
+  const max = Math.max(...values);
+  return values.map((v) => (v - min) / (max - min));
 };
 
 const update = () => {
-  const source = Math.floor(Math.random() * data.length);
-  const cx = Math.floor(Math.random() * data[source].length);
-  const cy = Math.floor(Math.random() * data[source].length);
+  source = Math.floor(Math.random() * data.length);
+  cx = Math.floor(Math.random() * data[source].length);
+  cy = Math.floor(Math.random() * data[source].length);
 
-  const xs = getNormalizedValues(source, cx);
-  const ys = getNormalizedValues(source, cy);
+  const xs = normalize(data[source][cx]);
+  const ys = normalize(data[source][cy]);
 
   const ps = Array(data[source][0].length)
     .fill(null)
-    .map((_, i) => [width * xs[i], height * ys[i]] as const);
+    .map(
+      (_, i) =>
+        [
+          5 * blur + (width - 10 * blur) * xs[i],
+          5 * blur + (height - 10 * blur) * ys[i],
+        ] as const
+    );
 
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, width, height);
@@ -37,10 +47,25 @@ const update = () => {
   ctx.beginPath();
   ps.forEach((p, i) => (i === 0 ? ctx.moveTo(...p) : ctx.lineTo(...p)));
   ctx.stroke();
+
+  ctx.drawImage(canvas, 0, 0);
 };
 
-document.addEventListener("keypress", (e) => {
-  if (e.key !== "f") return;
-  update();
+const save = () => {
+  const a = document.createElement("a");
+  a.href = canvas.toDataURL();
+  a.download = `blur.${blur}-${source}-${cx}-${cy}.png`;
+  a.click();
+};
+
+document.addEventListener("keypress", (evt) => {
+  switch (evt.key) {
+    case "f":
+      update();
+      break;
+    case "s":
+      save();
+      break;
+  }
 });
 update();
