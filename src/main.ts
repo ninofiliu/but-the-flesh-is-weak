dffadec01b3cb97496c92a415dbb4b61c839b7e3
Nino Filiu <nino.filiu@gmail.com>
2022-05-29 22:21:37 +0200

    skinnyness

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import untypedData from "./data.json";
 
 const width = 500;
 const height = 500;
-const blur = 3;
+const blur = 4;
+const skinny = 0.1;
 let source: number;
 let cx: number;
 let cy: number;
@@ -14,13 +15,22 @@ canvas.width = width;
 canvas.height = height;
 const ctx = canvas.getContext("2d")!;
 ctx.lineWidth = 2 * blur;
-ctx.filter = `blur(${blur}px) contrast(5)`;
+ctx.filter = `blur(${blur}px) brightness(0.6) contrast(5)`;
+document.body.style.background = "#eee";
 document.body.append(canvas);
 
 const normalize = (values: number[]) => {
   const min = Math.min(...values);
   const max = Math.max(...values);
   return values.map((v) => (v - min) / (max - min));
+};
+
+const interpolate = (cursor: number, values: number[]) => {
+  const t = cursor * values.length - 1;
+  const s = t % 1;
+  const iFrom = Math.floor(t);
+  const iTo = iFrom + 1;
+  return (1 - s) * values[iFrom] + s * values[iTo];
 };
 
 const update = () => {
@@ -31,15 +41,55 @@ const update = () => {
   const xs = normalize(data[source][cx]);
   const ys = normalize(data[source][cy]);
 
-  const ps = Array(data[source][0].length)
+  const { length } = data[source][0];
+  const ps = Array(length)
     .fill(null)
-    .map(
-      (_, i) =>
-        [
-          5 * blur + (width - 10 * blur) * xs[i],
-          5 * blur + (height - 10 * blur) * ys[i],
-        ] as const
-    );
+    .map((_, i) => {
+      const cursor = i / length;
+      const xMeasured = xs[i];
+      const yMeasured = ys[i];
+      const xProjMin = interpolate(cursor, [
+        0,
+        skinny,
+        1,
+        skinny,
+        0,
+        skinny,
+        1,
+      ]);
+      const xProjMax = interpolate(cursor, [
+        0,
+        1 - skinny,
+        1,
+        1 - skinny,
+        0,
+        1 - skinny,
+        1,
+      ]);
+      const yProjMin = interpolate(cursor, [
+        0,
+        skinny,
+        0,
+        skinny,
+        1,
+        skinny,
+        1,
+      ]);
+      const yProjMax = interpolate(cursor, [
+        0,
+        1 - skinny,
+        0,
+        1 - skinny,
+        1,
+        1 - skinny,
+        1,
+      ]);
+      const xPoint = xProjMin + (xProjMax - xProjMin) * xMeasured;
+      const yPoint = yProjMin + (yProjMax - yProjMin) * yMeasured;
+      const xCanvas = 5 * blur + (width - 10 * blur) * xPoint;
+      const yCanvas = 5 * blur + (height - 10 * blur) * yPoint;
+      return [xCanvas, yCanvas] as const;
+    });
 
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, width, height);
