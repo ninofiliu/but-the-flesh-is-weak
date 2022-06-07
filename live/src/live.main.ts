69fc1739b8b55688a3f307e1610004aae4a50a0d
Nino Filiu <nino.filiu@gmail.com>
2022-06-07 18:32:53 +0200

    creatPLotter

--- live/src/live.main.ts
+++ live/src/live.main.ts
@@ -1,12 +1,19 @@
-import salome from "./sounds/salome.mp3";
+import createMockReader from "./createMockReader";
+import createPlotter from "./createPlotter";
+// import salome from "./sounds/salome.mp3";
 
-const ac = new AudioContext();
-console.log(ac.state);
-const audio = document.createElement("audio");
-audio.src = salome;
-audio.autoplay = true;
-const source = ac.createMediaElementSource(audio);
-const gain = ac.createGain();
-gain.gain.value = 0.5;
-source.connect(gain);
-gain.connect(ac.destination);
+const plotter = createPlotter();
+createMockReader((port, evt) => {
+  plotter.data = evt;
+});
+
+// const ac = new AudioContext();
+// console.log(ac.state);
+// const audio = document.createElement("audio");
+// audio.src = salome;
+// audio.autoplay = true;
+// const source = ac.createMediaElementSource(audio);
+// const gain = ac.createGain();
+// gain.gain.value = 0.5;
+// source.connect(gain);
+// gain.connect(ac.destination);
