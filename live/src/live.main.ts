e2f39fd54e2328a84ea312314d0012f0cb56611f
Nino Filiu <nino.filiu@gmail.com>
2022-06-09 01:48:18 +0200

    plot smoothed out humidity

--- live/src/live.main.ts
+++ live/src/live.main.ts
@@ -1,7 +1,34 @@
 export {};
 
+const canvas = document.createElement("canvas");
+canvas.style.border = "1px solid black";
+canvas.style.display = "block";
+document.body.append(canvas);
+const ctx = canvas.getContext("2d")!;
+let x = 0;
+const plot = (values: { [color: string]: number }) => {
+  ctx.fillStyle = "white";
+  ctx.fillRect(x, 0, 1, canvas.height);
+  for (const color in values) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, canvas.height * (1 - values[color]), 1, 1);
+  }
+  x = (x + 1) % canvas.width;
+};
+
+let hum = 0;
+const humSmooth = 0.95;
+const humMin = 980;
+const humMax = 1024;
+
 const onData = (ns: number[]) => {
-  console.log(ns.join("\n"));
+  console.log(...ns);
+  hum =
+    humSmooth * hum + ((1 - humSmooth) * (ns[0] - humMin)) / (humMax - humMin);
+  plot({
+    blue: hum,
+    red: ns[1] / 1024,
+  });
 };
 
 const startButton = document.createElement("button");
