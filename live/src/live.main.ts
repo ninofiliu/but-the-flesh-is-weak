27bf6b6ec52f8c037a7f3d89ebe7737f6e9c060e
Nino Filiu <nino.filiu@gmail.com>
2022-06-09 10:46:25 +0200

    start

--- live/src/live.main.ts
+++ live/src/live.main.ts
@@ -2,8 +2,24 @@ import listen from "./listen";
 import plot from "./plot";
 import bgSrc from "./sounds/amo/composition.mp3";
 import humSrc from "./sounds/amo/ii. humility 2.mp3";
+import fastSrc from "./sounds/amo/i. fast touch.mp3";
+import _ from "lodash";
 
 const ac = new AudioContext();
+
+const createStart = async (src: string, wait: number) => {
+  const resp = await fetch(src);
+  const arrayBuffer = await resp.arrayBuffer();
+  const audioBuffer = await ac.decodeAudioData(arrayBuffer);
+  const start = () => {
+    console.log("started");
+    const source = ac.createBufferSource();
+    source.buffer = audioBuffer;
+    source.connect(ac.destination);
+    source.start();
+  };
+  return _.throttle(start, wait);
+};
 
 const createGain = (src: string) => {
   const elt = document.createElement("audio");
@@ -18,10 +34,12 @@ const createGain = (src: string) => {
 };
 
 const bgGain = createGain(bgSrc);
-const humGain = createGain(humSrc);
-
 bgGain.gain.value = 1;
+
+const humGain = createGain(humSrc);
 humGain.gain.value = 0;
+
+const startFast = await createStart(fastSrc, 1000 / 2);
 
 let hum = 0;
 const humSmooth = 0.95;
@@ -39,4 +57,11 @@ listen((ns: number[]) => {
     red: ns[1] / 1024,
   });
   humGain.gain.value = Math.max(0, (hum - 0.2) / 0.8);
+});
+
+// DEBUG
+document.addEventListener("keydown", (evt) => {
+  if (evt.key !== " ") return;
+  console.log("key down");
+  startFast();
 });
