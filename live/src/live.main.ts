f27790c48de593b23ffb3aa674203f2c66df416f
Nino Filiu <nino.filiu@gmail.com>
2022-06-09 02:38:43 +0200

    play liquid on hum sensor up

--- live/src/live.main.ts
+++ live/src/live.main.ts
@@ -1,17 +1,42 @@
 import listen from "./listen";
 import plot from "./plot";
+import bgSrc from "./sounds/amo/composition.mp3";
+import humSrc from "./sounds/amo/ii. humility 2.mp3";
+
+const ac = new AudioContext();
+
+const createGain = (src: string) => {
+  const elt = document.createElement("audio");
+  elt.src = src;
+  elt.autoplay = true;
+  elt.loop = true;
+  const source = ac.createMediaElementSource(elt);
+  const gain = ac.createGain();
+  source.connect(gain);
+  gain.connect(ac.destination);
+  return gain;
+};
+
+const bgGain = createGain(bgSrc);
+const humGain = createGain(humSrc);
+
+bgGain.gain.value = 1;
+humGain.gain.value = 0;
 
 let hum = 0;
 const humSmooth = 0.95;
-const humMin = 980;
-const humMax = 1024;
+const humBase = 1024;
+const humTop = 980;
 
 listen((ns: number[]) => {
   console.log(...ns);
   hum =
-    humSmooth * hum + ((1 - humSmooth) * (ns[0] - humMin)) / (humMax - humMin);
+    humSmooth * hum +
+    ((1 - humSmooth) * (ns[0] - humBase)) / (humTop - humBase);
   plot({
     blue: hum,
+    steelblue: 0.2,
     red: ns[1] / 1024,
   });
+  humGain.gain.value = Math.max(0, (hum - 0.2) / 0.8);
 });
