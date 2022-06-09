e31be1f3c605bdfb3e0af4b3c6319ffa895892e7
Nino Filiu <nino.filiu@gmail.com>
2022-06-09 16:13:47 +0200

    move to main

--- live/src/requestPorts.main.ts
+++
@@ -1,9 +0,0 @@
-export {};
-
-const button = document.createElement("button");
-button.textContent = "Request ports";
-button.addEventListener("click", async () => {
-  await navigator.serial.requestPort();
-  console.log("ok");
-});
-document.body.append(button);
