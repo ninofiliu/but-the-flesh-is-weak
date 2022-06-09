f5f57dbfbfbbf454544f591baba1407c394bc53c
Nino Filiu <nino.filiu@gmail.com>
2022-06-09 11:27:04 +0200

    add controls

--- live/src/listen.ts
+++ live/src/listen.ts
@@ -1,49 +1,42 @@
-export default (onData: (nbs: number[]) => any) => {
-  const startButton = document.createElement("button");
-  startButton.textContent = "start";
-  document.body.append(startButton);
-  startButton.addEventListener("click", async () => {
-    const decoder = new TextDecoder();
-    const [port] = await navigator.serial.getPorts();
-    // const port = await navigator.serial.requestPort();
-    await port.open({ baudRate: 9600 });
-    if (!port.readable) throw new Error("can not read");
+export default async (port: SerialPort, onData: (nbs: number[]) => any) => {
+  await port.open({ baudRate: 9600 });
+  if (!port.readable) throw new Error("can not read");
 
-    const reader = port.readable.getReader();
+  const decoder = new TextDecoder();
+  const reader = port.readable.getReader();
 
-    let willStop = false;
-    const stopButton = document.createElement("button");
-    stopButton.textContent = "stop";
-    document.body.append(stopButton);
-    stopButton.addEventListener("click", () => {
-      willStop = true;
-    });
+  let willStop = false;
+  const stopButton = document.createElement("button");
+  stopButton.textContent = "stop";
+  document.body.append(stopButton);
+  stopButton.addEventListener("click", () => {
+    willStop = true;
+  });
 
-    let nStr = "";
-    let nbs = [];
+  let nStr = "";
+  let nbs = [];
 
-    while (true) {
-      const { value, done } = await reader.read();
-      if (done || willStop) {
-        reader.releaseLock();
-        history.go(0);
-      }
-      for (const char of decoder.decode(value)) {
-        if (char === "," || char === "\n") {
-          nbs.push(+nStr);
-          nStr = "";
-          if (char === "\n") {
-            if (nbs.length === 2) {
-              onData(nbs);
-            } else {
-              console.log("garbage data:", nbs);
-            }
-            nbs = [];
+  while (true) {
+    const { value, done } = await reader.read();
+    if (done || willStop) {
+      reader.releaseLock();
+      history.go(0);
+    }
+    for (const char of decoder.decode(value)) {
+      if (char === "," || char === "\n") {
+        nbs.push(+nStr);
+        nStr = "";
+        if (char === "\n") {
+          if (nbs.length === 2 && nbs.every((n) => !Number.isNaN(n))) {
+            onData(nbs);
+          } else {
+            console.log("garbage data:", nbs);
           }
-        } else {
-          nStr += char;
+          nbs = [];
         }
+      } else {
+        nStr += char;
       }
     }
-  });
+  }
 };
--- live/src/live.main.ts
+++ live/src/live.main.ts
@@ -1,5 +1,4 @@
 import listen from "./listen";
-import plot from "./plot";
 import bgSrc from "./sounds/amo/composition.mp3";
 import humSrc from "./sounds/amo/ii. humility 2.mp3";
 import fastSrc from "./sounds/amo/i. fast touch.mp3";
@@ -12,7 +11,6 @@ const createStart = async (src: string, wait: number) => {
   const arrayBuffer = await resp.arrayBuffer();
   const audioBuffer = await ac.decodeAudioData(arrayBuffer);
   const start = () => {
-    console.log("started");
     const source = ac.createBufferSource();
     source.buffer = audioBuffer;
     source.connect(ac.destination);
@@ -41,27 +39,68 @@ humGain.gain.value = 0;
 
 const startFast = await createStart(fastSrc, 1000 / 2);
 
-let hum = 0;
-const humSmooth = 0.95;
-const humBase = 1024;
-const humTop = 980;
+const ports = await navigator.serial.getPorts();
+ports.forEach(async (port, portIndex) => {
+  let hum = 0;
 
-listen((ns: number[]) => {
-  console.log(...ns);
-  hum =
-    humSmooth * hum +
-    ((1 - humSmooth) * (ns[0] - humBase)) / (humTop - humBase);
-  plot({
-    blue: hum,
-    steelblue: 0.2,
-    red: ns[1] / 1024,
+  const params = {
+    humSmooth: 0.95,
+    humBase: 1024,
+    humTop: 980,
+    humThreshold: 0.2,
+  } as Record<string, number>;
+
+  for (const key in params) {
+    const line = document.createElement("div");
+    const desc = document.createElement("span");
+    desc.innerHTML = key;
+    const input = document.createElement("input");
+    input.type = "number";
+    input.value = `${params[key]}`;
+    input.addEventListener("input", () => {
+      params[key] = +input.value;
+      console.log(params);
+    });
+    line.append(desc, input);
+    document.body.append(line);
+  }
+
+  const canvas = document.createElement("canvas");
+  canvas.style.border = "1px solid black";
+  canvas.style.display = "block";
+  document.body.append(canvas);
+  const ctx = canvas.getContext("2d")!;
+
+  let x = 0;
+
+  const plot = (values: { [color: string]: number }) => {
+    ctx.fillStyle = "white";
+    ctx.fillRect(x, 0, 1, canvas.height);
+    for (const color in values) {
+      ctx.fillStyle = color;
+      ctx.fillRect(x, canvas.height * (1 - values[color]), 1, 1);
+    }
+    x = (x + 1) % canvas.width;
+  };
+
+  listen(port, (ns) => {
+    console.log(portIndex, ...ns);
+    const humNorm = (ns[0] - params.humBase) / (params.humTop - params.humBase);
+    hum = params.humSmooth * hum + (1 - params.humSmooth) * humNorm;
+    humGain.gain.value = Math.max(
+      0,
+      (hum - params.humThreshold) / (1 - params.humThreshold)
+    );
+    plot({
+      blue: hum,
+      steelblue: params.humThreshold,
+      red: ns[1] / 1024,
+    });
   });
-  humGain.gain.value = Math.max(0, (hum - 0.2) / 0.8);
 });
 
 // DEBUG
 document.addEventListener("keydown", (evt) => {
   if (evt.key !== " ") return;
-  console.log("key down");
   startFast();
 });
