9ef5e16ce4bbe729889c738817673eafb19b6cf6
Nino Filiu <nino.filiu@gmail.com>
2022-06-17 17:23:20 +0200

    default to 0.9 smoother

--- live/src/useSmoothed.ts
+++ live/src/useSmoothed.ts
@@ -1,7 +1,7 @@
 import { useRef, useState } from "react";
 
 export default (raw: number) => {
-  const [smoother, setSmoother] = useState(0.5);
+  const [smoother, setSmoother] = useState(0.9);
   const smoothed = useRef(0);
   smoothed.current = smoother * smoothed.current + (1 - smoother) * raw;
   return { smoothed, smoother, setSmoother };
