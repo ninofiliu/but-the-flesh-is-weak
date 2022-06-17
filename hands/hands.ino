2a4d2c80165464b02c0c2663016a33a273fc0583
Nino Filiu <nino.filiu@gmail.com>
2022-06-17 11:06:44 +0200

    hands 123

--- hands/hands.ino
+++ hands/hands.ino
@@ -3,5 +3,7 @@ void setup() { Serial.begin(9600); }
 void loop() {
   Serial.print(analogRead(A0));
   Serial.print(",");
-  Serial.println(analogRead(A1));
+  Serial.print(analogRead(A1));
+  Serial.print(",");
+  Serial.println(analogRead(A2));
 }
